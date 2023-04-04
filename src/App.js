9010c9a517e154c1276c669551ffa892f8acd261
vinicios.santos <viniciossantos@useall.com.br>
2023-04-04 14:01:43 -0300

    new home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
@@ -16,10 +16,10 @@ export default function App() {
       <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/" element={<Projects />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/company" element={<Company />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Container>
       <Footer/>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,3 +1,14 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+
 export default function Home() {
-    return <h1>Home</h1>
+    return(
+        <section className={styles.home_container}>
+            <h1>Welcome to <span>Costs</span></h1>
+            <p>Start gerence your projects now!</p>
+            <LinkButton to="/newproject/" text="Crate project"/>
+            <img src={savings} alt="Savings" />
+        </section>
+    )
 }
