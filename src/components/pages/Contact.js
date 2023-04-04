cf85c09a9d02fdb7b0c5d7336abb97d5557f088a
vinicios.santos <viniciossantos@useall.com.br>
2023-04-04 18:17:00 -0300

    Contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,3 +1,12 @@
-export default function Company() {
-    return <h1>Contact</h1>
+import person from '../../img/vinicios.png'
+import styles from './Contact.module.css'
+
+export default function Contact() {
+    return (
+    <div className={styles.contact}>
+    <h1>Contact</h1>
+    <h2>Vinicios Santos</h2>
+    <img src={person}/>
+    </div>
+    )
 }
