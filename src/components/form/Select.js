12d82d537a3e83fdc0e5bb26b9f79c6147e8cddd
vinicios.santos <viniciossantos@useall.com.br>
2023-04-05 13:41:55 -0300

    create and consume fetch api json server

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -6,6 +6,9 @@ export default function Select({ text, name, options, handleOnChange, value }) {
             <label htmlFor={name}>{text}</label>
             <select name={name} id={name}>
                 <option>Select option</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>{option.name}</option>
+                ))}
             </select>
         </div>
     )
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,15 +1,33 @@
+
+import { useState, useEffect } from 'react'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
+
 import styles from './ProjectForm.module.css'
 
-export default function projectForm({btnText}) {
+export default function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data)
+      })
+  }, [])
+
     return (
         <form className={styles.form}>
             <Input type="text" text="Project Name" name="name" placeholder="Project name" />
             <Input type="number" text="Total budget" name="name" placeholder="Total budget" />
-            <Select name="category_id" text="Select category"></Select>
-            <SubmitButton text={btnText}/>
+            <Select name="category_id" text="Select category" options={categories}></Select>
+            <SubmitButton text={btnText} />
         </form>
     )
 }
